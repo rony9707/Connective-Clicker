068d23da50a308836dacd098e9f517e6950f7084
rony9707 <chowdhury.agnibha.98@gmail.com>
2025-02-02 21:21:05 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+require('dotenv').config()
+
+const app = express();
+const server = http.createServer(app);
+
+//Use dot env
+frontEndConnectionString = process.env.frontEndConnectionString
+
+let onlineUsers = [];  // Track connected users
+
+// Socket.io CORS options
+const io = new Server(server, {
+  cors: {
+    origin: frontEndConnectionString,  // Allow Angular frontend to communicate with this backend
+    credentials: true,
+  }
+});
+
+// CORS middleware for HTTP requests
+app.use(cors({
+  origin: frontEndConnectionString,  // Allow Angular frontend to communicate with this backend
+  credentials: true,
+}));
+
+// Socket.io event listener
+io.on('connection', (client) => {
+
+  
+  client.on('username',(username)=>{
+    onlineUsers.push(username)
+    io.emit('allUsersOnline', onlineUsers); 
+  })
+
+  client.on('user-clicked',(userClicked)=>{
+    io.emit('user-clicked', userClicked); 
+  })
+
+});
+
+server.listen(9000, () => {
+  console.log("Server listening at port 9000");
+});
